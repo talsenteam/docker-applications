022dbd7c6e8312b3614ecdbd7f65450b66682ed7
David Lechner <david.lechner@talsen.team>
2021-06-10 16:31:25 +0200

    update maven project to use junit4

---
+++ docker/theia-ide/web/rootfs/etc/talsen/assets/java-template/src/test/java/team/talsen/kata/AppTest.java
@@ -0,0 +1,14 @@
+package team.talsen.kata;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest
+{
+    @Test
+    public void testApp()
+    {
+        assertThat( true ).isEqualTo( true );
+    }
+}
