2468c5d90208f4e07b5e5102bb94c63d2bf01b01
David Lechner <david.lechner@talsen.team>
2021-06-25 17:44:15 +0200

    test dojo build c workspace

---
+++ ansible/role/theia-ide/dojo-build-c/molecule/default/tests/test_requirement_1.py
@@ -0,0 +1,18 @@
+import datetime
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+now = datetime.datetime.now()
+date = now.strftime('%Y-%m-%d')
+
+
+def test_that_workspace_directory_exists(host):
+    d = host.file(f'/home/project/{date}-test/build')
+
+    assert d.exists
+    assert d.is_directory
