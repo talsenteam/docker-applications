1d3d3dbb285e6367859d217baa4dbc773186b166
David Lechner <david.lechner@talsen.team>
2021-06-25 14:28:52 +0200

    test dojo init plantuml

---
+++ ansible/role/theia-ide/dojo-init-plantuml/molecule/default/tests/test_requirement_1.py
@@ -0,0 +1,31 @@
+import datetime
+import pytest
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+now = datetime.datetime.now()
+date = now.strftime('%Y-%m-%d')
+
+
+def test_that_workspace_template_is_set(host):
+    f = host.file(f'/home/project/{date}-test/.workspace/.template')
+
+    assert f.exists
+    assert f.is_file
+    assert f.content_string == f'plantuml{os.linesep}'
+
+
+@pytest.mark.parametrize('name', [
+    'diagram.plantuml',
+])
+def test_that_template_specific_file_is_existing(host, name):
+    f = host.file(f'/home/project/{date}-test/{name}')
+
+    assert f.exists
+    assert f.is_file
+    assert f.size > 0
