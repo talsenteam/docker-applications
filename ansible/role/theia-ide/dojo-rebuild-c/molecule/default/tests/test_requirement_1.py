5cd6b3d0bb9f3a10669ab8216efb413e9456e90c
David Lechner <david.lechner@talsen.team>
2021-06-28 11:56:25 +0200

    test dojo rebuild c workspace

---
+++ ansible/role/theia-ide/dojo-rebuild-c/molecule/default/tests/test_requirement_1.py
@@ -0,0 +1,24 @@
+import datetime
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+now = datetime.datetime.now()
+date = now.strftime('%Y-%m-%d')
+
+
+def test_that_workspace_directory_exists(host):
+    d = host.file(f'/home/project/{date}-test/build')
+
+    assert d.exists
+    assert d.is_directory
+
+
+def test_that_build_directory_has_been_replaced(host):
+    f = host.file(f'/home/project/{date}-test/build/.marker')
+
+    assert not f.exists
