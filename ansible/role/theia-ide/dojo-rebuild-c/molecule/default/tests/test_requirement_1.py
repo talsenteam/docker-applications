286bc1bcc7032a6af189fd121d970ccb15ef49a8
David Lechner <david.lechner@talsen.team>
2021-06-28 22:07:23 +0200

    refactor test

--- ansible/role/theia-ide/dojo-rebuild-c/molecule/default/tests/test_requirement_1.py
+++ ansible/role/theia-ide/dojo-rebuild-c/molecule/default/tests/test_requirement_1.py
@@ -9,16 +9,17 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 now = datetime.datetime.now()
 date = now.strftime('%Y-%m-%d')
+workspace_dir = f'/home/project/{date}-test'
 
 
 def test_that_workspace_directory_exists(host):
-    d = host.file(f'/home/project/{date}-test/build')
+    d = host.file(f'{workspace_dir}/build')
 
     assert d.exists
     assert d.is_directory
 
 
 def test_that_build_directory_has_been_replaced(host):
-    f = host.file(f'/home/project/{date}-test/build/.marker')
+    f = host.file(f'{workspace_dir}/build/.marker')
 
     assert not f.exists
