4dab9e2fb3dee104cbeb0172047fbad1a9d466b4
David Lechner <david.lechner@talsen.team>
2021-06-28 13:40:21 +0200

    refine test

--- ansible/role/theia-ide/dojo-build-java/molecule/default/tests/test_requirement_1.py
+++ ansible/role/theia-ide/dojo-build-java/molecule/default/tests/test_requirement_1.py
@@ -10,14 +10,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 now = datetime.datetime.now()
 date = now.strftime('%Y-%m-%d')
+workspace_dir = f'/home/project/{date}-test'
 
 
 @pytest.mark.parametrize('name', [
     'target/classes',
     'target/test-classes',
 ])
-def test_that_workspace_directory_exists(host, name):
-    d = host.file(f'/home/project/{date}-test/{name}')
+def test_that_directory_exists(host, name):
+    d = host.file(f'{workspace_dir}/{name}')
 
     assert d.exists
     assert d.is_directory
