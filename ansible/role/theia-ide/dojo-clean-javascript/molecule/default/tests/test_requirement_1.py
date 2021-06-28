01762ed10aaaa3c928fb4ebffd6d18553f3c5196
David Lechner <david.lechner@talsen.team>
2021-06-28 11:42:49 +0200

    test dojo clean javascript workspace

---
+++ ansible/role/theia-ide/dojo-clean-javascript/molecule/default/tests/test_requirement_1.py
@@ -0,0 +1,17 @@
+import datetime
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+now = datetime.datetime.now()
+date = now.strftime('%Y-%m-%d')
+
+
+def test_that_workspace_directory_does_not_exist(host):
+    d = host.file(f'/home/project/{date}-test/coverage')
+
+    assert not d.exists
