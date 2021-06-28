233602aa79f0710022c05a24ffc9534dd87ddd72
David Lechner <david.lechner@talsen.team>
2021-06-28 21:48:43 +0200

    refactor test

--- ansible/role/theia-ide/dojo-new-workspace/molecule/default/tests/test_requirement_1.py
+++ ansible/role/theia-ide/dojo-new-workspace/molecule/default/tests/test_requirement_1.py
@@ -9,17 +9,18 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 now = datetime.datetime.now()
 date = now.strftime('%Y-%m-%d')
+workspace_dir = f'/home/project/{date}-test'
 
 
 def test_that_workspace_directory_exists(host):
-    d = host.file(f'/home/project/{date}-test')
+    d = host.file(f'{workspace_dir}')
 
     assert d.exists
     assert d.is_directory
 
 
 def test_that_workspace_name_is_set(host):
-    f = host.file(f'/home/project/{date}-test/.workspace/.name')
+    f = host.file(f'{workspace_dir}/.workspace/.name')
 
     assert f.exists
     assert f.is_file
