1016ebb36d71233f9f84dc252e114759d14fbdad
David Lechner <david.lechner@talsen.team>
2021-07-15 22:19:09 +0200

    add empty dojo and dojoctl commands

---
+++ docker/theia-ide-next/web/rootfs/etc/talsen/command/dojoctl/dummy.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -euo pipefail
+shopt -s inherit_errexit
+
+echo "Hello, world!"
---
+++ docker/theia-ide-next/web/rootfs/etc/talsen/dojo.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -euo pipefail
+shopt -s inherit_errexit
+
+COMMAND_DIR=/etc/talsen/command/dojo
+
+function print_help() {
+    for COMMAND in $( cd ${COMMAND_DIR} && ls *.bash )
+    do
+        /bin/bash ${COMMAND_DIR}/${COMMAND} --help
+
+        echo ""
+    done
+}
+
+function invoke_command() {
+    local COMMAND_SCRIPT=${COMMAND_DIR}/${1}.bash
+
+    if [ ! -f ${COMMAND_SCRIPT} ];
+    then
+        echo "Error: unknown command \"${1}\""
+
+        exit 1
+    fi
+
+    /bin/bash ${COMMAND_SCRIPT} ${@:2}
+}
+
+if [ ${#} = 0 ];
+then
+    print_help
+
+    exit 0
+fi
+
+invoke_command ${@:1}
